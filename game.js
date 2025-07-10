6333e01c083c4dd43e1510ad4a60a6a8e54af5fb
Jackob2004 <zjakub2004@gmail.com>
2025-07-10 15:12:55 +0200

    Implement play round functionality
    Human and computer scores are now stored.
    Round result is evaluated in playRound method and the winner is announced.
    Added helper method which capitalizes first char in a string and returns the result.
    Added helper method which returns winner message.

--- game.js
+++ game.js
@@ -1,3 +1,35 @@
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
+    console.log("Tie!");
+    return;
+  }
+
+  let playerWon = false;
+
+  if (humanChoice === "rock" && computerChoice === "scissors") {
+    playerWon = true;
+  }
+
+  if (humanChoice === "paper" && computerChoice === "rock") {
+    playerWon = true;  
+  }
+
+  if (humanChoice === "scissors" && computerChoice === "paper") {
+    playerWon = true;
+  }
+  
+  if (playerWon) {
+    humanScore++;
+  } else {
+    computerScore++;
+  }
+
+  console.log(getWinnerMessage(playerWon, humanChoice, computerChoice));  
+}
+
 function getComputerChoice() {
   let computerChoice;
   const randomNumber = Math.floor(Math.random() * 3); // 0-2
@@ -15,4 +47,23 @@ function getComputerChoice() {
   }
 
   return computerChoice;
+}
+// assumed player will enter a valid choice
+function getHumanChoice() {
+  let humanChoice = prompt("Choose one of these: rock, paper, scissors", "");
+  
+  return humanChoice.toLowerCase(); 
+}
+
+function getWinnerMessage(playerWon, humanChoice, computerChoice) {
+  humanChoice = capitalizeFirstLetter(humanChoice);
+  computerChoice = capitalizeFirstLetter(computerChoice);
+ 
+  return winnerMessage = (playerWon) ? 
+        "You Won! " + humanChoice + " beats " + computerChoice :
+        "You Lost! " + computerChoice + " beats " + humanChoice; 
+}
+
+function capitalizeFirstLetter(word) {
+  return word.charAt(0).toUpperCase() + word.substring(1);
 }
