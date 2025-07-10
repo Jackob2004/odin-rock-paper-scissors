c4d7c0c46219efed90ecc00f62df28e8d989c250
Jackob2004 <zjakub2004@gmail.com>
2025-07-10 12:07:55 +0200

    Implement generating computer choice
    Create getComputerChoice function which randomly returns computer choice.

--- game.js
+++ game.js
@@ -1,1 +1,18 @@
-console.log("Helo World");
+function getComputerChoice() {
+  let computerChoice;
+  const randomNumber = Math.floor(Math.random() * 3); // 0-2
+
+  switch(randomNumber) {
+    case 0:
+      computerChoice = "rock";
+      break;
+    case 1:
+      computerChoice = "paper";
+      break;
+    case 2:
+      computerChoice = "scissors";
+      break;
+  }
+
+  return computerChoice;
+}
